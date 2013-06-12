fdf7474b1616fb18c871e53c096a176724a3df76
Yevgen <godun.yevgen@gmail.com>
2013-06-12 17:59:20 +0300

    menu

--- js/common.js
+++ js/common.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
 
+// show/hide header nav
+    $(".js-key-nav").click(function(){
+        $(this).parent().toggleClass("is-active");
+        $(".js-nav").fadeToggle("fast");
+    });
+
 });
