9bcb65a4902565f33af71a71c3cd5d5e85ad9553
Yevgen <godun.yevgen@gmail.com>
2013-06-17 14:30:41 +0300

    tab

--- js/common.js
+++ js/common.js
@@ -6,7 +6,25 @@ $(document).ready(function() {
         $(".js-nav").fadeToggle("fast");
     });
 
+// Scrollpane
     $('.js-scrolpane').jScrollPane();
+
+// tabs
+	var tab = $(".js-tab li");
+	tab.first().addClass("is-active");
+	$(".js-tab1").show();
+	tab.click(function(){
+		var index = $(this).attr("data-tab");
+		if ($(this).hasClass("is-active")) {
+			return false;
+		}
+		else {
+			tab.removeClass("is-active");
+			$(this).addClass("is-active");
+			$(".tab-cont").hide();
+			$(index).show();
+		}
+	});
 
 
 });
