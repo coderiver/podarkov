faad30c22ed1e22b461f1d4c467665748891c6fc
Yevgen <godun.yevgen@gmail.com>
2013-10-25 11:21:14 +0300

    upd baron js

--- js/common.js
+++ js/common.js
@@ -96,38 +96,38 @@ $(document).ready(function() {
 // baron scroller
     function baron_init() {
         if ($(window).width() < 640) {
-            if ($(".js-baron1").length > 0) {
-                var scroll = baron({
-                    scroller: '.js-baron1',
-                    bar: '.scroller__bar',
-                    barOnCls: 'baron'
-                });
-                scroll.dispose();
-            }
-            if ($(".js-baron2").length > 0) {
-                var scroll = baron({
-                    scroller: '.js-baron2',
-                    bar: '.scroller__bar',
-                    barOnCls: 'baron'
-                });
-                scroll.dispose();
-            }
-            if ($(".js-baron3").length > 0) {
-                var scroll = baron({
-                    scroller: '.js-baron3',
-                    bar: '.scroller__bar',
-                    barOnCls: 'baron'
-                });
-                scroll.dispose();
-            }
-            if ($(".js-baron4").length > 0) {
-                var scroll = baron({
-                    scroller: '.js-baron4',
-                    bar: '.scroller__bar',
-                    barOnCls: 'baron'
-                });
-                scroll.dispose();
-            }
+            // if ($(".js-baron1").length > 0) {
+            //     var scroll = baron({
+            //         scroller: '.js-baron1',
+            //         bar: '.scroller__bar',
+            //         barOnCls: 'baron'
+            //     });
+            //     scroll.dispose();
+            // }
+            // if ($(".js-baron2").length > 0) {
+            //     var scroll = baron({
+            //         scroller: '.js-baron2',
+            //         bar: '.scroller__bar',
+            //         barOnCls: 'baron'
+            //     });
+            //     scroll.dispose();
+            // }
+            // if ($(".js-baron3").length > 0) {
+            //     var scroll = baron({
+            //         scroller: '.js-baron3',
+            //         bar: '.scroller__bar',
+            //         barOnCls: 'baron'
+            //     });
+            //     scroll.dispose();
+            // }
+            // if ($(".js-baron4").length > 0) {
+            //     var scroll = baron({
+            //         scroller: '.js-baron4',
+            //         bar: '.scroller__bar',
+            //         barOnCls: 'baron'
+            //     });
+            //     scroll.dispose();
+            // }
         }
         else {
             if ($(".js-baron1").length > 0) {
